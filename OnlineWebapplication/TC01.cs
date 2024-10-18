4b05f35c57efe29c71718fe8019fd75351e85f4c
ankitwits <ankitwits@gmail.com>
2024-10-18 21:04:10 +0530

    Added some more line of code

---
+++ OnlineWebapplication/TC01.cs
@@ -0,0 +1,43 @@
+using OpenQA.Selenium;
+using OnlineWebapplication.Factory;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using System.Reflection.Metadata;
+
+namespace OnlineWebapplication
+{
+    [TestClass]
+    public class TC01
+    {
+        IBrowser browserType;
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string browsertype = "chrome";
+
+           browserType = BrowserFactory.GetBrowser(browsertype);
+           IWebDriver driver = browserType.createDriver();
+            driver.Navigate().GoToUrl("https://techglory.godaddysites.com/");
+
+            SelectElement drpse = new SelectElement(driver.FindElement(By.Id("dropdownid")));
+
+            drpse.SelectByText("Test");
+
+            //ASSSert: how we perform validation
+
+            //Assert :
+            var element = driver.FindElements(By.Id("")).Count;
+            Assert.AreEqual(1, element);
+
+            var btninput = driver.FindElements(By.XPath("//input[@value='login']"));//input as tag, login as attribute
+
+            //if xpath ia an absolute path that is not the good way because that can change its better to
+            //put relative xpath will make sure that the property of the element does not changes.
+
+            
+
+        }
+    }
+}
