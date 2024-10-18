39ccba4a5848aa017ca6d1c308ad31e849008b97
ankitwits <ankitwits@gmail.com>
2024-10-18 15:35:57 +0530

    Initial commit

---
+++ OnlineWebapplication/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace OnlineWebapplication
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+using System.Configuration;
+using System.Reflection;
+
+namespace OnlineWebapplication
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string str = "";
+              
+        }
+    }
+}
